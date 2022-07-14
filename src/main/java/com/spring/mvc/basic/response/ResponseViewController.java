8fbc021806083bbb74b8ab3a21c8a1f5fd9c8490
Yoon <yoondev93@gmail.com>
2022-07-14 11:03:34 +0900

    feature: HTTP 응답 - 정적 리소스, 뷰 템플릿

---
+++ src/main/java/com/spring/mvc/basic/response/ResponseViewController.java
@@ -0,0 +1,32 @@
+package com.spring.mvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+  @RequestMapping("/response-view-v1")
+  public ModelAndView responseViewV1() {
+    ModelAndView mav = new ModelAndView("response/hello")
+        .addObject("data", "yoondev!");
+
+    return mav;
+  }
+
+  @RequestMapping("/response-view-v2")
+  public String responseViewV2(Model model) {
+    model.addAttribute("data", "yoondev!");
+
+    return "response/hello";
+  }
+
+  //권장 X
+  @RequestMapping("/response/hello")
+  public void responseViewV3(Model model) {
+    model.addAttribute("data", "hello!!");
+  }
+
+}
